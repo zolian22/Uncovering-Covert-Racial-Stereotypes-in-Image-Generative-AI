38048aa13d12542965ae38adc76289f6ff62e2ed
zolian22 <95266267+zolian22@users.noreply.github.com>
2026-06-04 11:49:26 +0800

    Uploade code

---
+++ un.js
@@ -0,0 +1,13 @@
+// Unknown
+const un = {
+  "login": "Sahkan log masuk",
+  "login_return": "Cuba semula",
+  "login_fail_desc": "Semak rangkaian anda dan cuba lagi",
+  "login_fail_title": "Permintaan gagal",
+  "login_success": "Log masuk berjaya",
+  "login_title": "Gunakan kod QR MangoTV untuk log masuk",
+  "login_btn": "Sahkan log masuk",
+  "login_desc2": "Sahkan bahawa anda yang melakukan operasi",
+  "login_desc1": "Untuk memastikan keselamatan akaun anda"
+};
+export default un;
